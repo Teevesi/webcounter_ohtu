204f6fffed13483bae6a0c5a40436f2b29036294
Touko <touko.ristiranta@gmail.com>
2025-11-14 18:16:36 +0200

    fix things

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -7,8 +7,8 @@ poetry run python3 src/index.py &
 
 echo "started Flask server"
 
-# odotetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5001)" != "200" ]];
+# odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000)" != "200" ]];
   do sleep 1;
 done
 
@@ -19,7 +19,7 @@ poetry run robot --variable HEADLESS:true src/tests
 
 status=$?
 
-# pysäytetään Flask-palvelin portissa 5001
+# pysäytetään Flask-palvelin portissa 5000
 kill $(lsof -t -i:5000)
 
 exit $status
